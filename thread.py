fac7a640bb3f2a7b0e0fc94642abbcaa5b156c52
Chriss78Vera <chrissvera75@gmail.com>
2022-07-12 18:40:29 -0500

    Prueba primer

---
+++ thread.py
@@ -0,0 +1,20 @@
+from time import sleep, perf_counter
+from threading import Thread
+def task(id):
+    print(f"Iniciando tarea {id}\n")
+    sleep(2)
+    print(f"Finalizando tarea {id}\n")
+    print(id*id*id)
+    
+start_time=perf_counter()
+threads=[]
+for n in range(1,200):
+    t=Thread(target=task, args=(n,))
+    threads.append(t)
+    t.start()
+    
+for t in threads:
+    t.join()
+    
+end_time=perf_counter()
+print(f"Tomo {end_time- start_time: }")
